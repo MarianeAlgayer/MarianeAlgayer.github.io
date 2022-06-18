6fb3d1808805e467346fa9892b15daf99ab3a7df
Mariane Algayer <marianealgayer@gmail.com>
2022-06-18 17:01:29 -0300

    feat: add mobile menu icon on header

--- portfolio/src/components/Header/index.jsx
+++ portfolio/src/components/Header/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { IoMenu } from 'react-icons/io5';
+
 import { Container, StyledLink } from './styles';
 
-function Header() {
+function Header({ setMenuIsVisible }) {
   return (
     <Container>
-      <StyledLink to="/">HOME</StyledLink>
-      <StyledLink to="/about">ABOUT</StyledLink>
-      <StyledLink to="/mariane-algayer-resume.pdf" target="_blank">RÉSUMÉ</StyledLink>
+      <nav>
+        <div className="menu-text">
+          <StyledLink to="/">HOME</StyledLink>
+          <StyledLink to="/about">ABOUT</StyledLink>
+          <StyledLink to="/mariane-algayer-resume.pdf" target="_blank">RÉSUMÉ</StyledLink>
+        </div>
+
+        <IoMenu onClick={ () => setMenuIsVisible(true) } />
+      </nav>
     </Container>
   );
 }
+
+Header.propTypes = {
+  setMenuIsVisible: PropTypes.func,
+}.isRequired;
 
 export default Header;
