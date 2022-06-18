6685cfe542858108e1c40c247b2f417c05127062
Mariane Algayer <marianealgayer@gmail.com>
2022-06-18 16:56:34 -0300

    feat: create mobile menu state on App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import RouteApp from './RouteApp';
 import Header from './components/Header';
+import MobileMenu from './components/MobileMenu';
 import SocialsMenu from './components/SocialsMenu';
 
 import { GlobalStyle } from './styles/global';
 
 function App() {
+  const [menuIsVisible, setMenuIsVisible] = useState(false);
+
   return (
     <>
       <GlobalStyle />
       <RouteApp />
 
-      <Header />
+      <Header
+        setMenuIsVisible={ setMenuIsVisible }
+      />
+
+      <MobileMenu
+        menuIsVisible={ menuIsVisible }
+        setMenuIsVisible={ setMenuIsVisible }
+      />
+
       <SocialsMenu />
     </>
   );
